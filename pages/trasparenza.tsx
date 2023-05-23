44f23eb89567dd97b8236c5128c83ca1a864c132
Paolo Zanotti <60442209+zanottipaolo@users.noreply.github.com>
2023-05-23 13:24:53 +0200

    📝 Add "5x1000 2021"

--- pages/trasparenza.tsx
+++ pages/trasparenza.tsx
@@ -9,37 +9,37 @@ import NavButton from "@/components/NavButton"
 const list = [
   {
     id: 0,
-    year: 2021,
+    year: 2015,
     file: [
       {
-        name: "Contrbuto 5x1000",
-        href: "https://drive.google.com/file/d/1UWDG3RpNgs6vJi4uh3tiDGwyvuP7PrHf/view?usp=sharing",
+        name: "Contributo 5x1000",
+        href: "https://drive.google.com/file/d/1bLTJ0eyYBPb9koJwvQzW7gPl2ppLw7nm/view?usp=sharing",
       },
       {
         name: "Dichiarazione 5x1000",
-        href: "https://drive.google.com/file/d/1IMvdhhzZHHHRWY1ANwR0LWj1Rqe7QFU9/view?usp=sharing",
+        href: "https://drive.google.com/file/d/1lqWFLXZnv37jDjiQPgqQeJMRkzhiq7gK/view?usp=sharing",
       },
     ],
     style: "bg-gray-100 dark:bg-gray-700",
   },
   {
     id: 1,
-    year: 2020,
+    year: 2016,
     file: [
       {
         name: "Contributo 5x1000",
-        href: "https://drive.google.com/file/d/1-8zh41IjWHOi576P6xRGvPEno3pEJw4T/view?usp=sharing",
+        href: "https://drive.google.com/file/d/12yb1HZb9PBZ-atiDG4cs-leIx5TnUyph/view?usp=sharing",
       },
     ],
     style: "",
   },
   {
     id: 2,
-    year: 2019,
+    year: 2017,
     file: [
       {
         name: "Contributo 5x1000",
-        href: "https://drive.google.com/file/d/1XzOAq5m73CATPVYxR3vkEexUo6DCdHJy/view?usp=sharing",
+        href: "https://drive.google.com/file/d/1QCFexIWpevX_JYdpRZDvjk4OvYaVyNeB/view?usp=sharing",
       },
     ],
     style: "bg-gray-100 dark:bg-gray-700",
@@ -57,37 +57,45 @@ const list = [
   },
   {
     id: 4,
-    year: 2017,
+    year: 2019,
     file: [
       {
         name: "Contributo 5x1000",
-        href: "https://drive.google.com/file/d/1QCFexIWpevX_JYdpRZDvjk4OvYaVyNeB/view?usp=sharing",
+        href: "https://drive.google.com/file/d/1XzOAq5m73CATPVYxR3vkEexUo6DCdHJy/view?usp=sharing",
       },
     ],
     style: "bg-gray-100 dark:bg-gray-700",
   },
   {
     id: 5,
-    year: 2016,
+    year: 2020,
     file: [
       {
         name: "Contributo 5x1000",
-        href: "https://drive.google.com/file/d/12yb1HZb9PBZ-atiDG4cs-leIx5TnUyph/view?usp=sharing",
+        href: "https://drive.google.com/file/d/1-8zh41IjWHOi576P6xRGvPEno3pEJw4T/view?usp=sharing",
+      },
+      {
+        name: "Dettaglio operazioni 5x1000",
+        href: "https://drive.google.com/file/d/13U6FPrRxmDZxQkjgJcs1Vq1sQyR88WTa/view?usp=sharing",
       },
     ],
     style: "",
   },
   {
     id: 6,
-    year: 2015,
+    year: 2021,
     file: [
       {
-        name: "Contributo 5x1000",
-        href: "https://drive.google.com/file/d/1bLTJ0eyYBPb9koJwvQzW7gPl2ppLw7nm/view?usp=sharing",
+        name: "Dichiarazione scuola 5x1000",
+        href: "https://drive.google.com/file/d/1UWDG3RpNgs6vJi4uh3tiDGwyvuP7PrHf/view?usp=sharing",
       },
       {
         name: "Dichiarazione 5x1000",
-        href: "https://drive.google.com/file/d/1lqWFLXZnv37jDjiQPgqQeJMRkzhiq7gK/view?usp=sharing",
+        href: "https://drive.google.com/file/d/1IMvdhhzZHHHRWY1ANwR0LWj1Rqe7QFU9/view?usp=sharing",
+      },
+      {
+        name: "Rendiconto 5x1000",
+        href: "https://drive.google.com/file/d/1crfSOlqMsRxsBqmDfUkrkR7UvlByWEaL/view?usp=sharing",
       },
     ],
     style: "bg-gray-100 dark:bg-gray-700",
