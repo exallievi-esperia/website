69f59ce978880ca7b8e94b97d87c5d77c14e24ad
Paolo Zanotti <60442209+zanottipaolo@users.noreply.github.com>
2023-06-07 14:18:13 +0200

    🍱 Add Rivista 2011

--- pages/rivista.tsx
+++ pages/rivista.tsx
@@ -44,69 +44,76 @@ const riviste = [
   },
   {
     id: 5,
+    year: 2011,
+    link: "",
+    imgAlt: "Anteprima rivista 2011",
+    imgSrc: "/img/copertine/Copertina2011.jpg",
+  },
+  {
+    id: 6,
     year: 2012,
     link: "https://drive.google.com/file/d/1x7aqzCABo-eKVyS4lIh7liEO30ZzSoj2/view?usp=sharing",
     imgAlt: "Anteprima rivista 2012",
     imgSrc: "/img/copertine/Copertina2012.jpg",
   },
   {
-    id: 6,
+    id: 7,
     year: 2013,
     link: "https://drive.google.com/file/d/1HNIX2VM5PLv3ypMueKGEby51XgXQJDNF/view?usp=sharing",
     imgAlt: "Anteprima rivista 2013",
     imgSrc: "/img/copertine/Copertina2013.jpg",
   },
   {
-    id: 7,
+    id: 8,
     year: 2014,
     link: "https://drive.google.com/file/d/1tB3SBLGqBfxIO_TjREZ_QKuzx9s6wXOe/view?usp=sharing",
     imgAlt: "Anteprima rivista 2014",
     imgSrc: "/img/copertine/Copertina2014.jpg",
   },
   {
-    id: 8,
+    id: 9,
     year: 2015,
     link: "https://drive.google.com/file/d/1YjTz6ImJt1u_kuccY0GJHsUa-HnUHcXJ/view?usp=sharing",
     imgAlt: "Anteprima rivista 2015",
     imgSrc: "/img/copertine/Copertina2015.jpg",
   },
   {
-    id: 9,
+    id: 10,
     year: 2016,
     link: "https://drive.google.com/file/d/1NldOFLFx8znGfcUsFpRa75p3afJ-P87o/view?usp=sharing",
     imgAlt: "Anteprima rivista 2016",
     imgSrc: "/img/copertine/Copertina2016.jpg",
   },
   {
-    id: 10,
+    id: 11,
     year: 2017,
     link: "https://drive.google.com/file/d/1XcNrfMv1g9HYKlmlfxfBHrMgTVI9vfxb/view?usp=sharing",
     imgAlt: "Anteprima rivista 2017",
     imgSrc: "/img/copertine/Copertina2017.jpg",
   },
   {
-    id: 11,
+    id: 12,
     year: 2018,
     link: "https://drive.google.com/file/d/1oGYyXmk4B_UR0hrMLFwLPIRsE_lOvd1I/view?usp=sharing",
     imgAlt: "Anteprima rivista 2018",
     imgSrc: "/img/copertine/Copertina2018.jpg",
   },
   {
-    id: 12,
+    id: 13,
     year: 2019,
     link: "https://drive.google.com/file/d/1VNA2eEEeGocQ57ZL8EUEkpzbrwpGAsXn/view?usp=sharing",
     imgAlt: "Anteprima rivista 2019",
     imgSrc: "/img/copertine/Copertina2019.jpg",
   },
   {
-    id: 13,
+    id: 14,
     year: 2022,
     link: "https://drive.google.com/file/d/1Z9rdZl8w6R5QeycQoZGsr-5hMqwNiaDA/view?usp=sharing",
     imgAlt: "Anteprima rivista 2022",
     imgSrc: "/img/copertine/Copertina2022.jpg",
   },
   {
-    id: 14,
+    id: 15,
     year: 2023,
     link: "https://drive.google.com/file/d/1RL2X4MQM5oLfo5tuJkW3tPuKspnSnQw-/view?usp=sharing",
     imgAlt: "Anteprima rivista 2023",
@@ -115,7 +122,7 @@ const riviste = [
 ]
 
 const RivistaPage = () => {
-  const [currentItems, setCurrentItems] = useState(15)
+  const [currentItems, setCurrentItems] = useState(16)
 
   return (
     <Layout>
