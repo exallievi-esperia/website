59589488cd0f44e96d7afc6521dc45895fd75eaf
Paolo Zanotti <60442209+zanottipaolo@users.noreply.github.com>
2024-10-14 16:27:29 +0200

    Update Heading.tsx

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -36,12 +36,16 @@ const Heading: React.FC<HeadingProps> = ({
       {/* Overlay */}
       <div className='absolute w-full h-full inset-0 bg-[#eaf8ffe0] dark:bg-gray-800/[0.88]'></div>
 
-      <h1 className='font-bold sm:text-5xl text-4xl mb-5 dark:text-white break-words relative z-10'>
-        {title}
-      </h1>
-      <p className='text-xl dark:text-gray-100 break-words relative z-10'>
-        {description}
-      </p>
+      {!isHome && 
+        <>
+          <h1 className='font-bold sm:text-5xl text-4xl mb-5 dark:text-white break-words relative z-10'>
+            {title}
+          </h1>
+          <p className='text-xl dark:text-gray-100 break-words relative z-10'>
+            {description}
+          </p>
+        </>
+      }
       {children}
     </div>
   )
